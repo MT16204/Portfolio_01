e5951733b687fdf65964fc786d65ef5f616c64d9
Tuan <phamminhtuan16204@gmail.com>
2024-11-13 16:49:46 +0700

    Add <file>

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const typed = new Typed('.typing-text', {
+        strings: ['Frontend Developer'],  
+        typeSpeed: 50,                    
+        backSpeed: 60,                    
+        backDelay: 1000,                  
+        loop: true                       
+    });
+});
